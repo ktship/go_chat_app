c5fc2db0e60db86c32e17435ff2c85071961365e
k2h <k2h@valuecontroller.com>
2015-10-05 18:49:14 +0900

    [기본] 기본 코드 추가

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"sync"
+	"html/template"
+	"path/filepath"
+	"flag"
+)
+
+func main() {
+	var addr = flag.String("addr", ":8080", "The addr of ther application.")
+	flag.Parse()
+
+	r := newRoom()
+	http.Handle("/", &templateHandler{ filename: "chat.html" })
+	http.Handle("/room", r)
+	go r.run()
+
+	log.Println("Starting web server on", *addr)
+
+	if err := http.ListenAndServe(*addr, nil) ; err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
+type templateHandler struct {
+	once 		sync.Once
+	filename 	string
+	templ 		*template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates\\", t.filename)))
+	})
+	t.templ.Execute(w, r)
+}
